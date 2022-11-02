329e99c7855935192b02aa9c5a00b138367ab6df
John O'Halloran <john.ohalloran@telus.com>
2022-11-02 18:47:41 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,23 @@
+//EXAMPLE 1
+/*
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+*/
+
+
+
+//EXAMPLE 2
+/*
 const temperature = 35;
 
 if (temperature < 0) {
@@ -9,3 +29,40 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+*/
+
+
+
+//EXAMPLE 3
+/*
+const isCitizen = true;
+const age = 26;
+
+
+
+//EXAMPLE 4
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+*/
+
+
+
+//EXAMPLE 5
+/*
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+*/
+
+
+
+
+//EXAMPLE 6
+/*
+let raining = false
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+*/
