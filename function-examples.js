bcfafc5c897b5ea5e037bf9a354f6c6c87f4b30f
John O'Halloran <john.ohalloran@telus.com>
2022-11-03 15:12:19 -0400

    adding function examples

---
+++ function-examples.js
@@ -0,0 +1,30 @@
+
+/*
+// Function expression that assigns the function displayFavorite 
+// to the variable favoriteMovie
+var favoriteMovie = function displayFavorite(movieName) {
+  console.log("My favorite movie is " + movieName);
+};
+
+// Function declaration that has two parameters: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+  messageFunction(name);
+}
+
+// Call the movies function, pass in the favoriteMovie function and name of movie
+movies(favoriteMovie, "Finding Nemo");
+*/
+
+
+/*
+var laugh = function (x){
+  var message = "";
+  for (var i = 1; i <= x; i++){
+      message = message + "ha";
+  }
+  return message + "!";
+}
+
+console.log(laugh(10));
+*/
