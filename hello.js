85668c53d9dd599576ab05b6046a640e3abeff6f
John O'Halloran <john.ohalloran@telus.com>
2022-11-03 17:14:47 -0400

    finished hello.js

--- hello.js
+++ hello.js
@@ -6,6 +6,7 @@ const sayHello  = function () {
 sayHello();
 */
 
+/*
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
@@ -13,3 +14,19 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+*/
+
+/*
+//console.log
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+*/
+
+//return
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
