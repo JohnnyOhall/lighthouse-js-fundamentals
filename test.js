f3d563fcc279f4848c908722e869676818949139
John O'Halloran <john.ohalloran@telus.com>
2022-11-03 00:20:17 -0400

    added test.js

---
+++ test.js
@@ -0,0 +1,33 @@
+// 11
+for (var x = 9; x >= 1; x--){
+  console.log("hello " + x);
+}
+
+
+//12
+for (var x = 5; x < 10; x++) {
+  console.log(x);
+}
+
+//13
+for (var k = 0; k < 200; k++) {
+  console.log(k);
+}
+
+
+//14
+let solution = 1;
+
+for (let i = 1; i <= 12; i++) {
+  solution *= i;
+}
+
+console.log(solution);
+
+
+//15
+for (var x = 0; x <= 25; x++){
+  for (var y = 0; y <= 99; y++){
+      return(x+"-"+y);
+  }
+}
